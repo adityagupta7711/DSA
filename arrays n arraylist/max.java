5f39ea2fd4621206c415bc1cf08254c3a1bc2590
Aditya Gupta <guptaaditya1231@gmail.com>
2024-01-23 12:18:16 +0530

    change

--- arrays n arraylist/max.java
+++ arrays n arraylist/max.java
@@ -1,7 +1,7 @@
 public class max {
     public static void main(String[] args) {
         int[] arr = {1, 3, 2, 9, 18};
-        System.out.println(maxRange(arr, 1, 3));
+        System.out.println(maxRange (arr, 1, 3));
     }
 
     // work on edge cases here, like array being null
