0f0e1e79917aabc905790b99fdf765ec765ab318
Aditya Gupta <guptaaditya1231@gmail.com>
2024-02-04 19:47:56 +0530

    new file

---
+++ sorting/selectionSort.java
@@ -0,0 +1,39 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class selectionSort {
+    public static void main(String[] args) {
+        int[] arr = { 8, 2, 4, 5, 3, 7 };
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int lastIndex = arr.length - i - 1;
+            // now we can find index of max element to be swapped with lastIndex
+            int maxIndex = max(arr, lastIndex);
+            // now we swap the max element with last index
+            swap(arr, maxIndex, lastIndex);
+        }
+
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static int max(int[] arr, int end) {
+        int start = 0;
+        int max = 0;
+        for (int i = start; i <= end; i++) {
+            if (arr[i] > arr[max]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+}
