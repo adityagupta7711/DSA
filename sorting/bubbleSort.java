8007f2b771b1a5b122c20131406d47a012ebbf9e
Aditya Gupta <guptaaditya1231@gmail.com>
2024-02-04 17:44:56 +0530

    new file

--- sorting/bubbleSort.java
+++ sorting/bubbleSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class bubbleSort {
     public static void main(String[] args) {
-        int[] arr = { 5, 4, 3, 2, 1 };
+        int[] arr = { 1, 2, 3, 4, 5 };
         bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
