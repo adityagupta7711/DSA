1e45f37205a4ecfb3422718a42aa3e6a3e1234d4
Aditya Gupta <guptaaditya1231@gmail.com>
2024-02-20 23:16:32 +0530

    new file

--- recursion/reverse.java
+++ recursion/reverse.java
@@ -12,6 +12,10 @@ public class reverse {
         rev1(n / 10);
     }
 
+    static void rev2(int n){
+        
+    }
+
     public static void main(String[] args) {
         rev1(1234);
         System.out.println(sum);
