5a61eb9ea349a428894d335ca4bc8fd05ee750d7
Aditya Gupta <guptaaditya1231@gmail.com>
2024-02-22 00:17:40 +0530

    new file

--- recursion/fibonacci.java
+++ recursion/fibonacci.java
@@ -3,6 +3,11 @@ public class fibonacci {
 public static void main(String[] args) {
     int ans= fibo(4);
     System.out.println(ans);
+    System.out.println(fibFormula(50));
+}
+static int fibFormula(int n){
+    return (int) ( Math.pow( ( (1 + Math.sqrt(5) ) /2 ) , n ) / Math.sqrt(5) );
+    // Use long instead of int
 }
     static int fibo(int n){
         //base condition
