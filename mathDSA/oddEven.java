35ba3d98b12fda37f302a9fb4644cf4142c688f7
Aditya Gupta <guptaaditya1231@gmail.com>
2024-02-26 16:24:17 +0530

    new file

---
+++ mathDSA/oddEven.java
@@ -0,0 +1,12 @@
+package mathDSA;
+
+public class oddEven {
+    public static void main(String[] args) {
+     int n = 69;
+     System.out.println(isOdd(n));   
+    }
+
+    private static boolean isOdd(int n) {
+        return (n & 1)==1;
+    }
+}
