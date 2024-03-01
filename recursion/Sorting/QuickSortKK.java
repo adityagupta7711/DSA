64a83357bdcf9489ae78eab16ccf164efcf693ee
Aditya Gupta <guptaaditya1231@gmail.com>
2024-03-01 22:28:53 +0530

    new file

--- recursion/Sorting/QuickSortKK.java
+++ recursion/Sorting/QuickSortKK.java
@@ -7,7 +7,7 @@ public class QuickSortKK {
         int[] arr = {5, 4, 3, 2, 1};
        sort(arr, 0, arr.length - 1);
        System.out.println(Arrays.toString(arr));
-        // Arrays.sort(arr);
+        // Arrays.sort(arr); //uses quick sort method
 
     }
     static void sort(int[] nums, int low, int hi) {
