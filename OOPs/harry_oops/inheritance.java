48684d51f62e79abe6c764015d6ac338e1ef2b58
Aditya Gupta <guptaaditya1231@gmail.com>
2024-05-19 16:57:55 +0530

    new file

--- OOPs/harry_oops/inheritance.java
+++ OOPs/harry_oops/inheritance.java
@@ -27,6 +27,7 @@ class Derived extends Base{
         this.y = y;
     }
 }
+        //multiple inheritance not there in JAvA
 
 public class inheritance {
     public static void main(String[] args) {
@@ -38,5 +39,50 @@ public class inheritance {
         Derived d= new Derived();
         d.setY(44);
         System.out.println(d.getY());
+        // pehle base class ka constructor run hota hain phir derived class ka constr
+        // argument ke sath wala constructor print krwane ke liye use 'super()' keyword
+        /*
+         * package com.company;
+
+class Base1{
+    Base1(){
+        System.out.println("I am a constructor");
+    }
+    Base1(int x){
+        System.out.println("I am an overloaded constructor with value of x as: " + x);
+    }
+}
+
+class Derived1 extends Base1{
+    Derived1(){
+        //super(0);
+        System.out.println("I am a derived class constructor");
+    }
+    Derived1(int x, int y){
+        super(x);
+        System.out.println("I am an overloaded constructor of Derived with value of y as: " + y);
+    }
+}
+
+class ChildOfDerived extends  Derived1{
+    ChildOfDerived(){
+        System.out.println("I am a child of derived constructor");
+    }
+    ChildOfDerived(int x, int y, int z){
+        super(x, y);
+        System.out.println("I am an overloaded constructor of Derived with value of z as: " + z);
+    }
+}
+public class cwh_46_constructors_in_inheritance {
+    public static void main(String[] args) {
+        // Base1 b = new Base1();
+        // Derived1 d = new Derived1();
+        // Derived1 d = new Derived1(14, 9);
+        // ChildOfDerived cd = new ChildOfDerived();
+        ChildOfDerived cd = new ChildOfDerived(12, 13, 15);
+    }
+}
+
+         */
     }
 }
